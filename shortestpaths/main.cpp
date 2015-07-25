eeba77ba95572f94d2b64424c92485639b576cd0
Ralph Gonzalez <ralphgonz@gmail.com>
2015-07-25 08:29:04 -0400

    more cleanup

--- shortestpaths/main.cpp
+++ shortestpaths/main.cpp
@@ -6,10 +6,9 @@
 
 using namespace std;
 
-class Point {
-    public:
-    long x;
-    long y;
+struct Point {
+	long x;
+	long y;
 };
 inline bool operator==(const Point& lhs, const Point& rhs){ return lhs.x == rhs.x && lhs.y == rhs.y; }
 inline bool operator!=(const Point& lhs, const Point& rhs){return !(lhs == rhs);}
@@ -17,52 +16,56 @@ inline bool operator!=(const Point& lhs, const Point& rhs){return !(lhs == rhs);
 enum Direction {LEFT, RIGHT, UP, DOWN};
 ostream &operator<<(ostream &os, Direction const &d) {
 	switch (d) {
-	case LEFT: os << "LEFT" ; break;
-	case RIGHT: os << "RIGHT" ; break;
-	case UP: os << "UP" ; break;
-	case DOWN: os << "DOWN" ; break;
+		case LEFT: os << "LEFT" ; break;
+		case RIGHT: os << "RIGHT" ; break;
+		case UP: os << "UP" ; break;
+		case DOWN: os << "DOWN" ; break;
 	}
-    return os;
+	return os;
 }
 
 class Path {
-   public:
-    vector<Direction> steps;
+private:
+	vector<Direction> steps;
 
-    Path() {}
-    Path(vector<Direction> _steps) : steps(_steps) {}
-    Path(const Path& path) : steps(path.steps) {}
-    void addStep(Direction direction);
+public:
+	Path() {}
+	Path(vector<Direction> _steps) : steps(_steps) {}
+	Path(const Path& path) : steps(path.steps) {}
+	void addStep(Direction direction);
+	friend ostream& operator<<(ostream&, const Path&);
 };
 void Path::addStep(Direction direction) {
-    steps.push_back(direction);
+	steps.push_back(direction);
 }
 ostream &operator<<(ostream &os, Path const &path) {
-    for (vector<Direction>::const_iterator i=path.steps.begin() ; i != path.steps.end() ; ++i) {
-        os << *i << ",";
-    }
+	for (vector<Direction>::const_iterator i=path.steps.begin() ; i != path.steps.end() ; ++i) {
+		os << *i << ",";
+	}
 	os << endl;
-    return os;
+	return os;
 }
 
 class Route {
-public:
+private:
 	Path path;
 	Point position;
 	Point end;
+
+public:
 
 	Route(const Path& _path, const Point& _position, const Point& _end) : path(_path), position(_position), end(_end) {}
 	void move(Direction direction);
 	void printAllShortestPaths(ostream& os);
 };
 void Route::move(Direction direction) {
-    path.addStep(direction);
-    switch (direction) {
-    case RIGHT: ++position.x; break;
-    case LEFT: --position.x; break;
-    case UP: ++position.y; break;
-    case DOWN: --position.y; break;
-    }
+	path.addStep(direction);
+	switch (direction) {
+		case RIGHT: ++position.x; break;
+		case LEFT: --position.x; break;
+		case UP: ++position.y; break;
+		case DOWN: --position.y; break;
+	}
 }
 void Route::printAllShortestPaths(ostream &os) {
 	if (position == end) {
@@ -93,13 +96,13 @@ void Route::printAllShortestPaths(ostream &os) {
 }
 
 int main() {
-    Point start;
-    Point end;
-    cin >> start.x;
-    cin >> start.y;
-    cin >> end.x;
-    cin >> end.y;
+	Point start;
+	Point end;
+	cin >> start.x;
+	cin >> start.y;
+	cin >> end.x;
+	cin >> end.y;
 
-    Route route(Path(), start, end);
-    route.printAllShortestPaths(cout);
+	Route route(Path(), start, end);
+	route.printAllShortestPaths(cout);
 }
